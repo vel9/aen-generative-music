8b0db153fd1f6560755f3b15c9851f0b17ff6af4
Levan Iordanishvili <levandev9053@gmail.com>
2017-02-12 11:33:51 -0500

    refactor harvest sources

--- src/main/java/com/vel9/generativemusic/pieces/harvest/melodysources/HarvestKwaenggMelodySource.java
+++ src/main/java/com/vel9/generativemusic/pieces/harvest/melodysources/HarvestKwaenggMelodySource.java
@@ -1,8 +1,10 @@
-package com.vel9.generativemusic.pieces.three.melodysources;
+package com.vel9.generativemusic.pieces.harvest.melodysources;
 
 import com.vel9.generativemusic.core.MelodySource;
 import com.vel9.generativemusic.core.dynamics.DynamicsStrategy;
-import com.vel9.generativemusic.core.pitch.*;
+import com.vel9.generativemusic.core.pitch.NoteStrategy;
+import com.vel9.generativemusic.core.pitch.Scale;
+import com.vel9.generativemusic.core.pitch.ScaleStrategy;
 import com.vel9.generativemusic.core.time.RhythmStrategy;
 import com.vel9.generativemusic.core.time.RhythmicSequence;
 import com.vel9.generativemusic.core.time.TempoStrategy;
@@ -17,37 +19,31 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * Created by levani on 12/12/16.
+ * Created by levani on 12/25/16.
  */
-public class ThreeCopenhagenMelodySource {
+public class HarvestKwaenggMelodySource {
 
     public static MelodySource getMelody(){
-        ScaleStrategy scaleStrategy = StrategyFactory.getScaleStrategy(getScales(36, 87), Util.getSeconds(5));
-        NoteStrategy noteStrategy = StrategyFactory.getNoteStrategy(scaleStrategy);
+        ScaleStrategy scaleStrategy = StrategyFactory.getSameScaleStrategy(getScale(72, 73, 75, 76));
+        NoteStrategy noteStrategy = StrategyFactory.getRandomNoteStrategy(scaleStrategy);
 
-        TempoStrategy tempoStrategy = StrategyFactory.getUpwardTempoStrategy(10, 25, Util.getSeconds(9));
-        DynamicsStrategy dynamicsStrategy = StrategyFactory.getUpwardDynamicsStrategy(2, 45, Util.getSeconds(1));
+        TempoStrategy tempoStrategy = StrategyFactory.getUpwardTempoStrategy(50, 80, Util.getSeconds(2));
+        DynamicsStrategy dynamicsStrategy = StrategyFactory.getRandomDynamicsStrategy(10, 80, Util.getSeconds(3));
         RhythmStrategy rhythmStrategy = StrategyFactory.getRhythmStrategy(tempoStrategy, dynamicsStrategy, getRhythmicSequences());
         return new SimpleMelodySource(noteStrategy, rhythmStrategy);
-    }
-
-    private static Scale[] getScales(int minNote, int maxNote){
-        return new Scale[]{new Scale(BaseScale.MODE3, NoteType.C, minNote, maxNote),
-                new Scale(BaseScale.MINOR, NoteType.D, minNote, maxNote),
-                new Scale(BaseScale.MINOR, NoteType.F_SHARP, minNote, maxNote),
-                new Scale(BaseScale.MINOR, NoteType.A_FLAT, minNote, maxNote),
-                new Scale(BaseScale.MODE3, NoteType.C, minNote, maxNote),
-                new Scale(BaseScale.MODE3, NoteType.D_SHARP, minNote, maxNote),
-                new Scale(BaseScale.MINOR, NoteType.B_FLAT, minNote, maxNote)
-        };
     }
 
     private static List<RhythmicSequence> getRhythmicSequences(){
         List<RhythmicSequence> rhythmicSequences = new ArrayList<>();
         rhythmicSequences.addAll(Arrays.asList(DeciTalaRhythmicSequence.values()));
-        for (int i = 0; i < 15; i++) {
+        for (int i = 0; i < 30; i++) {
             rhythmicSequences.add(SilentRhythmicSequence.QUADRUPLE_WHOLE_SILENCE);
+            rhythmicSequences.add(SilentRhythmicSequence.TRIPLE_WHOLE_SILENCE);
         }
         return rhythmicSequences;
+    }
+
+    private static Scale getScale(int... noteVals){
+        return Scale.createNonTransposingScale(noteVals);
     }
 }
