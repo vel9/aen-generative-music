33a3139ed1f320164b9f4ff3111402dd91a1c60f
Levan Iordanishvili <levandev9053@gmail.com>
2018-03-31 17:00:24 -0400

    Add modes 1 and 4

--- src/main/java/com/vel9/generativemusic/aen/core/pitch/BaseScale.java
+++ src/main/java/com/vel9/generativemusic/aen/core/pitch/BaseScale.java
@@ -14,8 +14,10 @@ public enum BaseScale {
     MIXOLYDIAN(NoteType.C, NoteType.D, NoteType.E, NoteType.F, NoteType.G, NoteType.A, NoteType.B_FLAT),
     LOCRIAN(NoteType.C, NoteType.D_FLAT, NoteType.E_FLAT, NoteType.F, NoteType.G_FLAT, NoteType.A_FLAT, NoteType.B_FLAT),
 
+    MODE1(NoteType.C, NoteType.D, NoteType.E, NoteType.F_SHARP, NoteType.G_SHARP, NoteType.A_SHARP),
     MODE2(NoteType.C, NoteType.C_SHARP, NoteType.D_SHARP, NoteType.E, NoteType.F_SHARP, NoteType.G, NoteType.A, NoteType.A_SHARP),
     MODE3(NoteType.C, NoteType.D, NoteType.E_FLAT, NoteType.E, NoteType.F_SHARP, NoteType.G, NoteType.A_FLAT, NoteType.B_FLAT, NoteType.B),
+    MODE4(NoteType.C, NoteType.D_FLAT, NoteType.D, NoteType.F, NoteType.F_SHARP, NoteType.G, NoteType.A_FLAT, NoteType.B),
     MODE6(NoteType.C, NoteType.D, NoteType.E, NoteType.F, NoteType.F_SHARP, NoteType.G_SHARP, NoteType.A_SHARP, NoteType.B);
 
     private NoteType[] noteTypes;
