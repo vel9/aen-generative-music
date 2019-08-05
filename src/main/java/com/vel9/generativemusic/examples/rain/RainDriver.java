343b79bc33049f90661a3425d735243f87777555
Levan Iordanishvili <levandev9053@gmail.com>
2019-08-05 08:21:06 -0400

    Update example piece class name

--- src/main/java/com/vel9/generativemusic/examples/rain/RainDriver.java
+++ src/main/java/com/vel9/generativemusic/examples/rain/RainDriver.java
@@ -4,9 +4,10 @@ import com.vel9.generativemusic.aen.core.perform.Instrument;
 import com.vel9.generativemusic.examples.rain.melodysources.RainHangDrumMKOneMelodySource;
 import com.vel9.generativemusic.examples.rain.melodysources.RainMarimbaMelodySource;
 
-public class RainMain {
+public class RainDriver {
 
     public static void main(String[] args) {
+        // routes MIDI signals to channel 0, 1 etc.
         Instrument.play(RainMarimbaMelodySource.getMelody(), 0);
         Instrument.play(RainMarimbaMelodySource.getMelody(), 1);
         Instrument.play(RainMarimbaMelodySource.getMelody(), 2);
